0340e429edaa234e7716b40bb7fdf436be761763
XinZouS <sanking.saphira@gmail.com>
2017-04-10 18:12:32 -0400

    Basic UI setup

---
+++ assignmentMovieDB/MovieCell.swift
@@ -0,0 +1,31 @@
+//
+//  MovieCell.swift
+//  assignmentMovieDB
+//
+//  Created by Xin Zou on 4/10/17.
+//  Copyright © 2017 Xin Zou. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UICollectionViewCell {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setupCell()
+        
+    }
+    
+    func setupCell(){
+        self.backgroundColor = .yellow
+    }
+    
+    
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+
